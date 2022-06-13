12243c31892ce3634d8fc6713be8a8ccc1b30579
Andrew Kraken <took77git@list.ru>
2022-06-13 13:09:07 +0300

    add lv_button_demo()

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <lvgl.h>
 #include "lv_conf.h"
 #include <WT32settings.h>
+#include <Buttons_demo.h>
 // место для инклуда нового Демо.h
 
 void setup() {
   Serial.begin(115200); /* prepare for possible serial debug */
   DisplayInit();
   // вызов функции нового демо из Демо.h
+  lv_button_demo();
 }
 
 void loop() {
